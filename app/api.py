9fcbceb5533200bf5b1d8ab399c6c32d6f747655
Fernando Contreras Pardo <fc7430@gmail.com>
2023-12-03 19:55:09 -0500

    act 2 changes added

--- app/api.py
+++ app/api.py
@@ -1,33 +1,109 @@
 import http.client
-
 from flask import Flask
-
 from app import util
 from app.calc import Calculator
 
+# Creación de la instancia de la calculadora
 CALCULATOR = Calculator()
+# Creación de la aplicación Flask
 api_application = Flask(__name__)
+# Definición de los encabezados HTTP comunes
 HEADERS = {"Content-Type": "text/plain", "Access-Control-Allow-Origin": "*"}
 
 
 @api_application.route("/")
 def hello():
+    """
+    Ruta raíz que devuelve un mensaje de bienvenida.
+    """
     return "Hello from The Calculator!\n"
+
+
+def process_operation(operation, op_1, op_2=None):
+    """
+    Función auxiliar para procesar operaciones de la calculadora.
+    
+    Parámetros:
+    operation -- Función de operación de la calculadora a ejecutar.
+    op_1 -- Primer operando como string.
+    op_2 -- Segundo operando como string (opcional, dependiendo de la operación).
+    
+    Retorna:
+    Una tupla de tres elementos: el resultado de la operación, el código HTTP y los encabezados.
+    """
+    try:
+        # Conversión de los operandos a números
+        num_1 = util.convert_to_number(op_1)
+        num_2 = util.convert_to_number(op_2) if op_2 is not None else None
+
+        # Ejecución de la operación
+        result = operation(num_1, num_2) if num_2 is not None else operation(num_1)
+
+        # Devolver el resultado con un código HTTP OK
+        return ("{}".format(result), http.client.OK, HEADERS)
+    except ZeroDivisionError:
+        return "División por cero no es posible", http.client.BAD_REQUEST
+    except (TypeError, ValueError) as e:
+        # Devolver el mensaje de error con un código HTTP BAD_REQUEST
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
 
 
 @api_application.route("/calc/add/<op_1>/<op_2>", methods=["GET"])
 def add(op_1, op_2):
-    try:
-        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
-        return ("{}".format(CALCULATOR.add(num_1, num_2)), http.client.OK, HEADERS)
-    except TypeError as e:
-        return (str(e), http.client.BAD_REQUEST, HEADERS)
+    """
+    Ruta para sumar dos números.
+    """
+    return process_operation(CALCULATOR.add, op_1, op_2)
 
 
 @api_application.route("/calc/substract/<op_1>/<op_2>", methods=["GET"])
 def substract(op_1, op_2):
-    try:
-        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
-        return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
-    except TypeError as e:
-        return (str(e), http.client.BAD_REQUEST, HEADERS)
+    """
+    Ruta para restar dos números.
+    """
+    return process_operation(CALCULATOR.substract, op_1, op_2)
+
+
+@api_application.route("/calc/multiply/<op_1>/<op_2>", methods=["GET"])
+def multiply(op_1, op_2):
+    """
+    Ruta para multiplicar dos números.
+    """
+    return process_operation(CALCULATOR.multiply, op_1, op_2)
+
+
+@api_application.route("/calc/divide/<op_1>/<op_2>", methods=["GET"])
+def divide(op_1, op_2):
+    """
+    Ruta para dividir un número por otro.
+    """
+    return process_operation(CALCULATOR.divide, op_1, op_2)
+
+
+@api_application.route("/calc/power/<op_1>/<op_2>", methods=["GET"])
+def power(op_1, op_2):
+    """
+    Ruta para elevar un número a la potencia de otro.
+    """
+    return process_operation(CALCULATOR.power, op_1, op_2)
+
+
+@api_application.route("/calc/sqrt/<op_1>", methods=["GET"])
+def sqrt(op_1):
+    """
+    Ruta para calcular la raíz cuadrada de un número.
+    """
+    return process_operation(CALCULATOR.sqrt, op_1)
+
+
+@api_application.route("/calc/log/<op_1>/<base>", methods=["GET"])
+def log(op_1, base):
+    """
+    Ruta para calcular el logaritmo de un número en una base dada.
+    """
+    return process_operation(CALCULATOR.log, op_1, base)
+
+
+if __name__ == "__main__":
+    # Ejecución de la aplicación Flask
+    api_application.run(debug=True)
--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -1,23 +1,96 @@
 import http.client
+import json
 import os
 import unittest
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
 
 import pytest
 
+# Base URL del servicio API para la calculadora, obtenida de las variables de entorno
 BASE_URL = os.environ.get("BASE_URL")
-DEFAULT_TIMEOUT = 2  # in secs
-
+DEFAULT_TIMEOUT = 2  # Tiempo de espera por defecto para las respuestas de la API, en segundos
 
 @pytest.mark.api
 class TestApi(unittest.TestCase):
+    # Pruebas de éxito
     def setUp(self):
-        self.assertIsNotNone(BASE_URL, "URL no configurada")
-        self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
+        # Asegurar que la URL base esté configurada antes de cada prueba
+        self.assertIsNotNone(BASE_URL, "URL base no configurada")
+        self.assertTrue(len(BASE_URL) > 8, "URL base parece inválida")
 
     def test_api_add(self):
+        # Probar la función de suma de la API
         url = f"{BASE_URL}/calc/add/2/2"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
+        self.assertEqual(response.status, http.client.OK, f"Error en la petición API a {url}")
+
+    # Prueba para la función de resta
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(response.status, http.client.OK)
+
+    # Prueba para la función de multiplicación
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(response.status, http.client.OK)
+
+    # Prueba para la función de división
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(response.status, http.client.OK)
+
+    # Prueba para la función de potencia
+    def test_api_power(self):
+        url = f"{BASE_URL}/calc/power/2/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(response.status, http.client.OK)
+
+    # Prueba para la función de raíz cuadrada
+    def test_api_sqrt(self):
+        url = f"{BASE_URL}/calc/sqrt/4"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(response.status, http.client.OK)
+
+    # Prueba para la función logarítmica
+    def test_api_log(self):
+        url = f"{BASE_URL}/calc/log/100/10"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(response.status, http.client.OK)
+    
+    # Pruebas de error
+    def test_api_divide_by_zero(self):
+        url = f"{BASE_URL}/calc/divide/10/0"
+        with self.assertRaises(HTTPError) as context:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(context.exception.code, http.client.BAD_REQUEST, "La API no maneja la división por cero correctamente")
+
+    def test_api_invalid_input(self):
+        urls = [
+            f"{BASE_URL}/calc/add/ten/2",
+            f"{BASE_URL}/calc/divide/10/two"
+        ]
+        for url in urls:
+            with self.assertRaises(HTTPError) as context:
+                urlopen(url, timeout=DEFAULT_TIMEOUT)
+            self.assertEqual(context.exception.code, http.client.BAD_REQUEST, "La API no maneja entradas inválidas correctamente")
+
+    # Prueba para la función de raíz cuadrada con número negativo
+    def test_api_sqrt_negative_number(self):
+        url = f"{BASE_URL}/calc/sqrt/-4"
+        with self.assertRaises(HTTPError) as context:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(context.exception.code, http.client.BAD_REQUEST, "La API no maneja la raíz cuadrada de números negativos correctamente")
+
+    # Prueba para la función logarítmica con base inválida
+    def test_api_log_invalid_base(self):
+        url = f"{BASE_URL}/calc/log/4/1"
+        with self.assertRaises(HTTPError) as context:
+            urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(context.exception.code, http.client.BAD_REQUEST, "La API no maneja la base logarítmica inválida correctamente")
+
+if __name__ == "__main__":
+    unittest.main()
