f354de5d2d8ce88b93bd516e0914e4146fcfa8b6
Fernando Contreras Pardo <fc7430@gmail.com>
2023-12-03 23:19:35 -0500

    add test corrected

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -19,7 +19,7 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.add(2, -2))
         self.assertEqual(0, self.calc.add(-2, 2))
         self.assertEqual(1, self.calc.add(1, 0))
-        self.assertEqual(2, self.calc.add(2, 2))
+        self.assertEqual(2, self.calc.add(1, 1))
         self.assertEqual(10, self.calc.add(5, 5))
 
 
